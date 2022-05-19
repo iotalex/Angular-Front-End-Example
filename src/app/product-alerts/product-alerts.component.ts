6a3d576f5730c1f668fa320360e7d8da2fee3c84
iotalex <alexfbaten@gmail.com>
2022-05-19 15:00:47 -0500

    1 file modified, configure output componenet for eventEmitter

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,8 +6,11 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+
+// onfiguring ProductAlertsComponent with an @Output() allows the ProductAlertsComponent to emit an event when the value of the notify property changes.
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
